9ce5fa2fa5882dde5a122ab4314f3fe752e428ec
lisahns <lisa.hanes@hotmail.de>
2022-07-04 11:26:59 +0200

    Challenge 2

--- Documents/FAC/FAC/Testing/learn-integration-testing/workshop/index.test.js
+++ Documents/FAC/FAC/Testing/learn-integration-testing/workshop/index.test.js
@@ -28,3 +28,24 @@ test("test for strings" , () => {
     const result = calculate("2", "+", "2")
     equal(expected, result)
 }); 
+//concatenates numbers
+
+//Challenge 2
+test("calculate user input and update the page" , () => {
+    const input1 = document.getElementById("a");
+    input1.value = "2";
+    const input2 = document.getElementById("b");
+    input2.value = "3";
+    const sign = document.querySelector("select");
+    sign.value = "+";
+    const submitButton = document.querySelector("button[type='submit']");
+     submitButton.click();
+     const result = document.querySelector("#result");
+     console.log(typeof(result));
+     equal(result.textContent, "5"); 
+     result.value = "";
+})
+
+//why are they passed as strings??
+//why is typeof object?
+//querySelector vs getElementById
